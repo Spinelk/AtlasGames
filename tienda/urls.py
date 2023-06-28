27f051a0c4614642d550b90df3385090353f68d6
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-06-28 04:28:17 -0400

    todas las vistas estan integradas a django

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path("", index, name="index"),
+    path("noticias", noticias, name="noticias"),
+    path("iniciosesion", inicio_sesion, name="iniciosesion"),
+    path("registro", registro, name="registro"),
+    path("juego", juego, name="juego"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'tienda/index.html')
+    return render(request, 'index.html')
+
+def noticias(request):
+    return render(request, 'noticias.html')
+
+def inicio_sesion(request):
+    return render(request, 'inicioSesion.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
+def juego(request):
+    return render(request, 'juego.html')
